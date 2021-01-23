e1ff8bc9e51108f18dcfe29b45d62050cffa609c
hanaf <hanaf.othmane@gmail.com>
2021-01-23 18:19:17 -0500

    updating the h1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useLayoutEffect, useState} from "react";
 
 function App() {
+
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event){
+    const {value, name} = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {fName: value, lName: prevValue.lName};
+      } else {
+        return {fName: prevValue.fName, lName: value};
+      }
+    })
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="fName" value={fullName.fName} placeholder="First Name" />
+        <input onChange={handleChange} name="lName" value={fullName.lName} placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
